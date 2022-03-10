52e1e214a8a111e0073801bfeb67bba47d1bd59e
Alisson Youssf <youssfbr@gmail.com>
2022-03-09 22:18:13 -0300

    Backend project created

---
+++ backend/src/main/java/com/github/youssfbr/cancerdemama/CancerdemamaApplication.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.cancerdemama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CancerdemamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CancerdemamaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/youssfbr/cancerdemama/CancerdemamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.cancerdemama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CancerdemamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
